d78cc107da167330f233c92a41864764a2bb73e3
sport <sport@JACOB_LAPTOP>
2023-03-06 19:21:37 -0500

    Add project files.

---
+++ Automatic Cursor Mover/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Automatic_Cursor_Mover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartBTN = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.setupBTN = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Automatic Cursor Mover";
+            // 
+            // StartBTN
+            // 
+            this.StartBTN.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartBTN.Location = new System.Drawing.Point(261, 205);
+            this.StartBTN.Name = "StartBTN";
+            this.StartBTN.Size = new System.Drawing.Size(115, 75);
+            this.StartBTN.TabIndex = 1;
+            this.StartBTN.Text = "Start ";
+            this.StartBTN.UseVisualStyleBackColor = true;
+            this.StartBTN.Click += new System.EventHandler(this.StartBTN_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(552, 48);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Press q to force quit program (program must be visible on \r\nscreen for  this to w" +
+    "ork so don\'t click of this program)";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 8;
+            // 
+            // setupBTN
+            // 
+            this.setupBTN.Font = new System.Drawing.Font("Arial Black", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.setupBTN.Location = new System.Drawing.Point(149, 90);
+            this.setupBTN.Name = "setupBTN";
+            this.setupBTN.Size = new System.Drawing.Size(348, 93);
+            this.setupBTN.TabIndex = 9;
+            this.setupBTN.Text = "Setup ";
+            this.setupBTN.UseVisualStyleBackColor = true;
+            this.setupBTN.Click += new System.EventHandler(this.setupBTN_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(592, 66);
+            this.label3.TabIndex = 10;
+            this.label3.Text = resources.GetString("label3.Text");
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.setupBTN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.StartBTN);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartBTN;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button setupBTN;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Automatic Cursor Mover/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Automatic_Cursor_Mover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern long SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        int x;
+        int y;
+        int RIGHTDOWN = 0x0002;
+        int RIGHTUP = 0x0004;
+
+
+        private bool is_On = false;
+        private int timerClock = -1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int j = 0;
+        private int movementPx = 100; //this is how far it will move right, up, left, and down
+
+        private async void StartBTN_Click(object sender, EventArgs e)
+        {
+            x = 1920 / 2 - 190;
+            y = 1080 / 2 - 150;
+            int delayTime = 63; //63 = 1 min rotation
+            int breakCounter = 0;
+            timerClock = 0;
+
+
+            if (myValue == "")
+            {
+                MessageBox.Show("You can't run the program without setting it up");
+            }
+
+            is_On = true;
+            if (is_On == true)
+            {
+                if ((string)myValue == "yes")
+                {
+                    do
+                    {
+                        //Moves the mouse right by 100px
+                        for (j = 0; j < movementPx; j++)
+                        {
+                            SetCursorPos(x + j, y);
+                            await Task.Delay(delayTime);
+                        }
+                        //Moves the mouse back to OG Postition
+                        for (j = movementPx - 1; j >= 0; j--)
+                        {
+                            SetCursorPos(x + j, y);
+                            await Task.Delay(delayTime);
+                        }
+
+                        mouse_event(RIGHTDOWN, 0, 0, 0, 0);
+
+                        //Moves the mouse up by 100px
+                        for (j = 0; j < movementPx; j++)
+                        {
+                            SetCursorPos(x, y + j);
+                            await Task.Delay(delayTime);
+                        }
+                        //Moves the mouse back to Og Position
+                        for (j = movementPx - 1; j >= 0; j--)
+                        {
+                            SetCursorPos(x, y + j);
+                            await Task.Delay(delayTime);
+                        }
+
+                        mouse_event(RIGHTUP, 0, 0, 0, 0);
+
+                        //Moves the mouse left by 100px
+                        for (j = 0; j < movementPx; j++)
+                        {
+                            SetCursorPos(x - j, y);
+                            await Task.Delay(delayTime);
+                        }
+
+                        mouse_event(RIGHTDOWN, 0, 0, 0, 0);
+
+                        //Moves the mouse back to OG Postition
+                        for (j = movementPx - 1; j >= 0; j--)
+                        {
+                            SetCursorPos(x - j, y);
+                            await Task.Delay(delayTime);
+                        }
+                        //Moves the mouse down by 100px
+                        for (j = 0; j < movementPx; j++)
+                        {
+                            SetCursorPos(x, y - j);
+                            await Task.Delay(delayTime);
+                        }
+
+                        mouse_event(RIGHTUP, 0, 0, 0, 0);
+
+                        //Moves the mouse back to Og Position
+                        for (j = movementPx - 1; j >= 0; j--)
+                        {
+                            SetCursorPos(x, y - j);
+                            await Task.Delay(delayTime);
+                        }
+                    } while (timerClock == 0);
+                }
+
+                else
+                {
+                    for (int i = 0; i < timeLength; i++)
+                    {
+                        //Moves the mouse right by 100px
+                        for (j = 0; j < movementPx; j++)
+                        {
+                            SetCursorPos(x + j, y);
+                            await Task.Delay(delayTime);
+                        }
+                        //Moves the mouse back to OG Postition
+                        for (j = movementPx - 1; j >= 0; j--)
+                        {
+                            SetCursorPos(x + j, y);
+                            await Task.Delay(delayTime);
+                        }
+
+                        mouse_event(RIGHTDOWN, 0, 0, 0, 0);
+
+                        //Moves the mouse up by 100px
+                        for (j = 0; j < movementPx; j++)
+                        {
+                            SetCursorPos(x, y + j);
+                            await Task.Delay(delayTime);
+                        }
+                        //Moves the mouse back to Og Position
+                        for (j = movementPx - 1; j >= 0; j--)
+                        {
+                            SetCursorPos(x, y + j);
+                            await Task.Delay(delayTime);
+                        }
+
+                        mouse_event(RIGHTUP, 0, 0, 0, 0);
+
+                        //Moves the mouse left by 100px
+                        for (j = 0; j < movementPx; j++)
+                        {
+                            SetCursorPos(x - j, y);
+                            await Task.Delay(delayTime);
+                        }
+
+                        mouse_event(RIGHTDOWN, 0, 0, 0, 0);
+
+                        //Moves the mouse back to OG Postition
+                        for (j = movementPx - 1; j >= 0; j--)
+                        {
+                            SetCursorPos(x - j, y);
+                            await Task.Delay(delayTime);
+                        }
+                        //Moves the mouse down by 100px
+                        for (j = 0; j < movementPx; j++)
+                        {
+                            SetCursorPos(x, y - j);
+                            await Task.Delay(delayTime);
+                        }
+
+                        mouse_event(RIGHTUP, 0, 0, 0, 0);
+
+                        //Moves the mouse back to Og Position
+                        for (j = movementPx - 1; j >= 0; j--)
+                        {
+                            SetCursorPos(x, y - j);
+                            await Task.Delay(delayTime);
+                        }
+
+                        breakCounter++;
+
+                        if(breakCounter == timeLength / breaks && breaks != 0)
+                        {
+                            for(int j = 0; j < breakLength * 60000; j += 5000)
+                            {
+                                mouse_event(RIGHTDOWN, 0, 0, 0, 0);
+                                await Task.Delay(breakLength * 5000);
+                                breakCounter = 0;
+                                mouse_event(RIGHTUP, 0, 0, 0, 0);
+                            }
+                            breakCounter = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void StopBTN_Click(object sender, EventArgs e)
+        {
+            is_On = false;
+        }
+    
+
+        private void TimerBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+                if (keyData==(Keys.Q))
+                {
+                    Application.Exit();
+                return true;
+                }
+                
+            return base.ProcessCmdKey(ref msg, keyData);    
+        }
+
+        private int timeLength = 0;
+        private int breaks = 0;
+        private int breakLength = 0;
+        private string message, title, myValue = "", myValue2 = "", myValue3 = "", myValue4 = "";
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setupBTN_Click(object sender, EventArgs e)
+        {
+            bool is_Allowed = false;
+
+            message = "Do you want to run the program forever? (yes to run it forever, and no to set a time)";
+
+            title = "Automatic Cursor Mover Setup";
+
+            
+            do
+            {
+                myValue = Interaction.InputBox(message, title);
+                myValue.ToLower();
+
+
+                if ((string)myValue == "yes")
+                {
+                    is_Allowed = true;
+                    MessageBox.Show("Press start to run the program forever, when ready to stop press Q to end program");
+                }
+                else if ((string)myValue == "no")
+                {
+                    is_Allowed = true;
+                    message = "How many minutes do you want to run the program for? (Not including time stopped for breaks)";
+                    title = "Automatic Cursor Mover Setup";
+
+                    myValue2 = Interaction.InputBox(message, title);
+                    timeLength = int.Parse(myValue2);
+
+
+                    message = "How many breaks do you want";
+                    title = "Automatic Cursor Mover Setup";
+
+                    myValue3 = Interaction.InputBox(message, title);
+                    breaks = int.Parse(myValue3);
+
+                    if(breaks != 0)
+                    {
+                        message = "How many minutes do you want the breaks to be";
+                        title = "Automatic Cursor Mover Setup";
+
+                        myValue4 = Interaction.InputBox(message, title);
+                        breakLength = int.Parse(myValue4);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("What you entered is not allowed, you can only enter yes or no");
+                }
+            } while (is_Allowed == false);
+        }
+    }
+}
